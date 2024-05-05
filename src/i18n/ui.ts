e8e1327bbd764455828cd106be623a675aa1922b
nyawox <nyawox.git@gmail.com>
2024-05-05 13:34:45 +0900

    translations: add a japanese translation

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,9 +1,11 @@
 import dk from "../../public/locales/dk/translations.json";
 import en from "../../public/locales/en/translations.json";
+import ja from "../../public/locales/ja/translations.json";
 
 export const languages = {
 	dk: "Dansk",
 	en: "English",
+	ja: "日本語",
 };
 
 export const defaultLang = "en";
@@ -11,4 +13,5 @@ export const defaultLang = "en";
 export const ui = {
 	dk,
 	en,
+	ja,
 } as const;
