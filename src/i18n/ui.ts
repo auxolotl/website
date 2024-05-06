d33cb45bef0435bd1c9d87440ff744ccab4ca7dd
~hedy <hedy.dev@protonmail.com>
2024-05-06 20:03:22 +0800

    translation: initial chinese translation
    translation: special interest groups for zh
    
    zh: translate all of goals
    
    zh: translate all values
    
    zh: translate soft and hard forks
    
    zh: translate roadmap org and infra
    
    zh: translate roadmap alignment stage
    
    zh: misc fixes
    
    zh: fix phrase for soft/hard forks
    
    Co-authored-by: Gen <buzhybrid@gmail.com>
    
    zh: adjust wording of root.description
    
    zh: adjust SIGs description
    
    zh: fix terminology use for 'upstream'

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,9 +1,11 @@
 import dk from "../../public/locales/dk/translations.json";
 import en from "../../public/locales/en/translations.json";
+import zh from "../../public/locales/zh/translations.json";
 
 export const languages = {
 	dk: "Dansk",
 	en: "English",
+	zh: "Chinese",
 };
 
 export const defaultLang = "en";
@@ -11,4 +13,5 @@ export const defaultLang = "en";
 export const ui = {
 	dk,
 	en,
+	zh,
 } as const;
