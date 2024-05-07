94eafb8dc61f28b3aaa3092cfb3825fc8b18923b
Emil "AngryAnt" Johansen <AngryAnt@users.noreply.github.com>
2024-05-07 12:45:54 +0200

    translations: init danish (#14)

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,11 +1,14 @@
+import dk from "../../public/locales/dk/translations.json";
 import en from "../../public/locales/en/translations.json";
 
 export const languages = {
+	dk: "Dansk",
 	en: "English",
 };
 
 export const defaultLang = "en";
 
 export const ui = {
+	dk,
 	en,
 } as const;
