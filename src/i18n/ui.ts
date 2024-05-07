34b69d119ebc6c27422216d7db52cf961fbb0464
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2024-05-07 00:25:26 +0000

    Update ui.ts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,11 +1,13 @@
 import en from "../../public/locales/en/translations.json"
 
 export const languages = {
+	de: 'Deutsch',
 	en: 'English'
 };
 
 export const defaultLang = 'en';
 
 export const ui = {
+	de,
 	en,
 } as const;
