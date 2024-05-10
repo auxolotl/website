7ab3741bc3dbe1932861286911d8c12175694da1
dantefromhell <dantefromhell@users.noreply.github.com>
2024-05-10 01:55:17 +0000

    fix syntax errors

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,6 +1,6 @@
-import de from "../../public/locales/de/translations.json"
+import de from "../../public/locales/de/translations.json";
 import dk from "../../public/locales/dk/translations.json";
-import en from "../../public/locales/en/translations.json"
+import en from "../../public/locales/en/translations.json";
 
 export const languages = {
 	de: 'Deutsch',
