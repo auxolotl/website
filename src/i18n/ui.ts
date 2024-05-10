8c84a59311956bf2f6ac22fe9353edae9d373163
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2024-05-10 14:38:58 +0000

    translations: init german (#20)
    * Update ui.ts
    
    * Fix ui.ts
    
    * translations: init german
    
    * translate captions
    
    * Translate goals section
    
    * translate values section
    
    * First round of feedback
    
    * Translate 'roadmap' section
    
    * Add clarification from Jake
    
    * 2nd round of reviews
    
    * fix syntax errors
    
    * satisfy prettier
    
    ---------
    
    Co-authored-by: dantefromhell <dantefromhell@users.noreply.github.com>

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,7 +1,9 @@
+import de from "../../public/locales/de/translations.json";
 import dk from "../../public/locales/dk/translations.json";
 import en from "../../public/locales/en/translations.json";
 
 export const languages = {
+	de: "Deutsch",
 	dk: "Dansk",
 	en: "English",
 };
@@ -9,6 +11,7 @@ export const languages = {
 export const defaultLang = "en";
 
 export const ui = {
+	de,
 	dk,
 	en,
 } as const;
